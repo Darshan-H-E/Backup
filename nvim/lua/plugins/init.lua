f1a5a0dfc0a594736796093999370059d0931575
Darshan Eshwarappa <darshanjhe@gmail.com>
2025-09-02 23:58:32 +0530

    leet

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -14,6 +14,19 @@ return {
   },
 
   {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+        -- include a picker of your choice, see picker section for more details
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+    },
+    opts = {
+        -- configuration goes here
+    },
+  },
+
+  {
     "MattesGroeger/vim-bookmarks",
     event = "VeryLazy",
     init = function()
